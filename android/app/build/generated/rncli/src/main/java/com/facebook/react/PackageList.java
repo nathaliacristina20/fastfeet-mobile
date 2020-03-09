458241fd718458d2e12b938ab1424c9998793fa9
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-03-08 23:22:55 -0300

    Atualizations android

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import com.fastfeet.BuildConfig;
 import com.fastfeet.R;
 
+// @react-native-community/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 // @react-native-community/masked-view
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 // react-native-gesture-handler
@@ -73,6 +75,7 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
+      new AsyncStoragePackage(),
       new RNCMaskedViewPackage(),
       new RNGestureHandlerPackage(),
       new LinearGradientPackage(),
